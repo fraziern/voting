237ccb43a337a161b20c11fa38b1c43e340af68b
Nick Frazier <nrflaw@gmail.com>
2016-08-30 08:17:39 -0400

    heroku build fix

--- _config.js
+++ _config.js
@@ -2,17 +2,12 @@ require('dotenv').config();
 
 var config = {};
 
-var mongoURIDev = process.env.MONGODB_URI;
+var mongoURIProd = process.env.MONGODB_URI;
 var mongoURITest = 'mongodb://localhost/test';
 
 config.mongoURI = {
-  development: mongoURIDev,
+  production: mongoURIProd,
   test: mongoURITest
-};
-
-config.prefixURL = {
-  development: 'http://slightly-shorter.herokuapp.com/',
-  test: 'http://localhost:8080/'
 };
 
 module.exports = config;
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-process.env.NODE_ENV = 'development';
-
 // env vers and related config vars
 require('dotenv').config();
 var config = require('./_config');
